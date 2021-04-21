8e83b5213898d29c2380d3014f819a9a2fab91bd
Nishant Nayak <nishantnayak2001@gmail.com>
2021-04-21 21:29:41 +0530

    Imported mvt.h into main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "include/Bankers.h"
 #include "include/cpu_algo.h"
 #include "include/reader_writer.h"
+#include "include/mvt.h"
 
 using namespace std;
 
@@ -12,6 +13,7 @@ int main() {
 	// Call bankers() to execute the bankers algorithm.
 	// Call CPU_Scheduling() to execute CPU Scheduling algos.
 	// Call reader_writer() to execute reader writer algorithm.
+	// Call mvt() to execute MVT memory management.
 
 	return 0;
 }
